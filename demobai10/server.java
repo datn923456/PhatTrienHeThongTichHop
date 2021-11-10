efc09c140e1d280cd3364fa251f811fe048a9c5f
datn923456 <datn923456@gmail.com>
2021-11-10 15:50:21 +0700

    tong hop 2

--- demobai10/server.java
+++ demobai10/server.java
@@ -3,11 +3,11 @@ package demobai10;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class sever {
+public class server {
     public static void main(String[] args) {
         try {
             ServerSocket ss=new ServerSocket(1111);
-            System.out.println("Sever dduoc tao");
+            System.out.println("Server dduoc tao");
             while(true)
             {
                 Socket s= ss.accept();
